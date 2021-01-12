68159aef2d903772c4fdef7dd07b4d09e7a526af
dganochenko <dganochenko.work@gmail.com>
2021-01-12 08:06:00 +0100

    add more palettes

--- src/App.svelte
+++ src/App.svelte
@@ -9,214 +9,182 @@
 		onMount
 	} from "svelte";
 
-	const palettes = [{
+	const palettes = [
+	  {
 			name: 'frozen-rose',
 			colors: ['#29368f', '#e9697b', '#1b164d', '#f7d996'],
 			background: '#f2e8e4',
+		}, {
+			name: 'sakura',
+			colors: ['#D30068', '#92EC00', '#00C12B', '#F0FC00'],
+			background: '#1B172A',
 		},
 		{
 			name: 'winter-night',
 			colors: ['#122438', '#dd672e', '#87c7ca', '#ebebeb'],
 			background: '#ebebeb',
-		},
-		{
+		}, {
 			name: 'saami',
 			colors: ['#eab700', '#e64818', '#2c6393', '#eecfca'],
 			background: '#e7e6e4',
-		},
-		{
+		}, {
 			name: 'knotberry1',
 			colors: ['#20342a', '#f74713', '#686d2c', '#e9b4a6'],
 			background: '#e5ded8',
-		},
-		{
+		}, {
 			name: 'knotberry2',
 			colors: ['#1d3b1a', '#eb4b11', '#e5bc00', '#f29881'],
 			background: '#eae2d0',
-		},
-		{
+		}, {
 			name: 'tricolor',
 			colors: ['#ec643b', '#56b7ab', '#f8cb57', '#1f1e43'],
 			background: '#f7f2df',
-		},
-		{
+		}, {
 			name: 'foxshelter',
 			colors: ['#ff3931', '#007861', '#311f27', '#bab9a4'],
 			background: '#dddddd',
-		},
-		{
+		}, {
 			name: 'hermes',
 			colors: ['#253852', '#51222f', '#b53435', '#ecbb51'],
 			background: '#eeccc2',
-		},
-		{
+		}, {
 			name: 'olympia',
 			colors: ['#ff3250', '#ffb33a', '#008c36', '#0085c6', '#4c4c4c'],
 			stroke: '#0b0b0b',
 			background: '#faf2e5',
-		},
-		{
+		}, {
 			name: 'byrnes',
 			colors: ['#c54514', '#dca215', '#23507f'],
 			stroke: '#0b0b0b',
 			background: '#e8e7d4',
-		},
-		{
+		}, {
 			name: 'butterfly',
 			colors: ['#f40104', '#f6c0b3', '#99673a', '#f0f1f4'],
 			stroke: '#191e36',
 			background: '#191e36',
-		},
-		{
+		}, {
 			name: 'floratopia',
 			colors: ['#bf4a2b', '#cd902a', '#4e4973', '#f5d4bc'],
 			stroke: '#1e1a43',
 			background: '#1e1a43',
-		},
-		{
+		}, {
 			name: 'verena',
 			colors: ['#f1594a', '#f5b50e', '#14a160', '#2969de', '#885fa4'],
 			stroke: '#1a1a1a',
 			background: '#e2e6e8',
-		},
-		{
+		}, {
 			name: 'florida_citrus',
 			colors: ['#ea7251', '#ebf7f0', '#02aca5'],
 			stroke: '#050100',
 			background: '#9ae2d3',
-		},
-		{
+		}, {
 			name: 'lemon_citrus',
 			colors: ['#e2d574', '#f1f4f7', '#69c5ab'],
 			stroke: '#463231',
 			background: '#f79eac',
-		},
-		{
+		}, {
 			name: 'yuma_punk',
 			colors: ['#f05e3b', '#ebdec4', '#ffdb00'],
 			stroke: '#ebdec4',
 			background: '#161616',
-		},
-		{
+		}, {
 			name: 'moir',
 			colors: ['#a49f4f', '#d4501e', '#f7c558', '#ebbaa6'],
 			stroke: '#161716',
 			background: '#f7f4ef',
-		},
-		{
+		}, {
 			name: 'sprague',
 			colors: ['#ec2f28', '#f8cd28', '#1e95bb', '#fbaab3', '#fcefdf'],
 			stroke: '#221e1f',
 			background: '#fcefdf',
-		},
-		{
+		}, {
 			name: 'bloomberg',
 			colors: ['#ff5500', '#f4c145', '#144714', '#2f04fc', '#e276af'],
 			stroke: '#000',
 			background: '#fff3dd',
-		},
-		{
+		}, {
 			name: 'revolucion',
 			colors: ['#ed555d', '#fffcc9', '#41b797', '#eda126', '#7b5770'],
 			stroke: '#fffcc9',
 			background: '#2d1922',
-		},
-		{
+		}, {
 			name: 'sneaker',
 			colors: ['#e8165b', '#401e38', '#66c3b4', '#ee7724', '#584098'],
 			stroke: '#401e38',
 			background: '#ffffff',
-		},
-		{
+		}, {
 			name: 'miradors',
 			colors: ['#ff6936', '#fddc3f', '#0075ca', '#00bb70'],
 			stroke: '#ffffff',
 			background: '#020202',
-		},
-		{
+		}, {
 			name: 'kaffeprat',
 			colors: ['#BCAA8C', '#D8CDBE', '#484A42', '#746B58', '#9A8C73'],
 			stroke: '#000',
 			background: '#fff',
-		},
-		{
+		}, {
 			name: 'rag-mysore',
 			colors: ['#ec6c26', '#613a53', '#e8ac52', '#639aa0'],
 			background: '#d5cda1'
-		},
-		{
+		}, {
 			name: 'rag-gol',
 			colors: ['#d3693e', '#803528', '#f1b156', '#90a798'],
 			background: '#f0e0a4'
-		},
-		{
+		}, {
 			name: 'rag-belur',
 			colors: ['#f46e26', '#68485f', '#3d273a', '#535d55'],
 			background: '#dcd4a6'
-		},
-		{
+		}, {
 			name: 'rag-bangalore',
 			colors: ['#ea720e', '#ca5130', '#e9c25a', '#52534f'],
 			background: '#f9ecd3'
-		},
-		{
+		}, {
 			name: 'rag-taj',
 			colors: ['#ce565e', '#8e1752', '#f8a100', '#3ac1a6'],
 			background: '#efdea2'
-		},
-		{
+		}, {
 			name: 'rag-virupaksha',
 			colors: ['#f5736a', '#925951', '#feba4c', '#9d9b9d'],
 			background: '#eedfa2'
-		},
-		{
+		}, {
 			name: 'tsu_arcade',
 			colors: ['#4aad8b', '#e15147', '#f3b551', '#cec8b8', '#d1af84', '#544e47'],
 			background: '#cfc7b9'
-		},
-		{
+		}, {
 			name: 'tsu_harutan',
 			colors: ['#75974a', '#c83e3c', '#f39140', '#e4ded2', '#f8c5a4', '#434f55'],
 			background: '#cfc7b9'
-		},
-		{
+		}, {
 			name: 'tsu_akasaka',
 			colors: ['#687f72', '#cc7d6c', '#dec36f', '#dec7af', '#ad8470', '#424637'],
 			background: '#cfc7b9'
-		},
-		{
+		}, {
 			name: 'hilda01',
 			colors: ['#ec5526', '#f4ac12', '#9ebbc1', '#f7f4e2'],
 			background: '#e7e8d4'
-		},
-		{
+		}, {
 			name: 'hilda02',
 			colors: ['#eb5627', '#eebb20', '#4e9eb8', '#f7f5d0'],
 			background: '#77c1c0'
-		},
-		{
+		}, {
 			name: 'hilda03',
 			colors: ['#e95145', '#f8b917', '#b8bdc1', '#ffb2a2'],
 			background: '#6b7752'
-		},
-		{
+		}, {
 			name: 'hilda04',
 			colors: ['#e95145', '#f6bf7a', '#589da1', '#f5d9bc'],
 			background: '#f5ede1'
-		},
-		{
+		}, {
 			name: 'empusa',
 			colors: ['#c92a28', '#e69301', '#1f8793', '#13652b', '#e7d8b0', '#48233b', '#e3b3ac'],
 			stroke: '#1a1a1a',
 			background: '#f0f0f2',
-		},
-		{
+		}, {
 			name: 'delphi',
 			colors: ['#475b62', '#7a999c', '#2a1f1d', '#fbaf3c', '#df4a33', '#f0e0c6', '#af592c'],
 			stroke: '#2a1f1d',
 			background: '#f0e0c6',
-		},
-		{
+		}, {
 			name: 'mably',
 			colors: [
 				'#13477b',
@@ -230,20 +198,17 @@
 			],
 			stroke: '#2a1f1d',
 			background: '#dfc792',
-		},
-		{
+		}, {
 			name: 'nowak',
 			colors: ['#e85b30', '#ef9e28', '#c6ac71', '#e0c191', '#3f6279', '#ee854e', '#180305'],
 			stroke: '#180305',
 			background: '#ede4cb',
-		},
-		{
+		}, {
 			name: 'jupiter',
 			colors: ['#c03a53', '#edd09e', '#aab5af', '#023629', '#eba735', '#8e9380', '#6c4127'],
 			stroke: '#12110f',
 			background: '#e6e2d6',
-		},
-		{
+		}, {
 			name: 'hersche',
 			colors: [
 				'#df9f00',
@@ -257,14 +222,12 @@
 			],
 			stroke: '#0a0a0a',
 			background: '#f0f5f6',
-		},
-		{
+		}, {
 			name: 'cherfi',
 			colors: ['#99cb9f', '#cfb610', '#d00701', '#dba78d', '#2e2c1d', '#bfbea2', '#d2cfaf'],
 			stroke: '#332e22',
 			background: '#e3e2c5',
-		},
-		{
+		}, {
 			name: 'harvest',
 			colors: [
 				'#313a42',
@@ -278,6 +241,98 @@
 			],
 			stroke: '#463930',
 			background: '#e5e2cf',
+		}, {
+			name: 'cc239',
+			colors: ['#e3dd34', '#78496b', '#f0527f', '#a7e0e2'],
+			background: '#e0eff0'
+		}, {
+			name: 'cc234',
+			colors: ['#ffce49', '#ede8dc', '#ff5736', '#ff99b4'],
+			background: '#f7f4ed'
+		}, {
+			name: 'cc232',
+			colors: ['#5c5f46', '#ff7044', '#ffce39', '#66aeaa'],
+			background: '#e9ecde'
+		}, {
+			name: 'cc238',
+			colors: ['#553c60', '#ffb0a0', '#ff6749', '#fbe090'],
+			background: '#f5e9de'
+		}, {
+			name: 'cc242',
+			colors: ['#bbd444', '#fcd744', '#fa7b53', '#423c6f'],
+			background: '#faf4e4'
+		}, {
+			name: 'cc245',
+			colors: ['#0d4a4e', '#ff947b', '#ead3a2', '#5284ab'],
+			background: '#f6f4ed'
+		}, {
+			name: 'cc273',
+			colors: ['#363d4a', '#7b8a56', '#ff9369', '#f4c172'],
+			background: '#f0efe2'
+		}, {
+			name: 'jud_playground',
+			colors: ['#f04924', '#fcce09', '#408ac9'],
+			background: '#ffffff'
+		}, {
+			name: 'jud_horizon',
+			colors: ['#f8c3df', '#f2e420', '#28b3d0', '#648731', '#ef6a7d'],
+			background: '#f2f0e1'
+		}, {
+			name: 'jud_mural',
+			colors: ['#ca3122', '#e5af16', '#4a93a2', '#0e7e39', '#e2b9bd'],
+			background: '#e3ded8'
+		}, {
+			name: 'jud_cabinet',
+			colors: ['#f0afb7', '#f6bc12', '#1477bb', '#41bb9b'],
+			background: '#e3ded8'
+		}, {
+			name: 'kov_01',
+			colors: ['#d24c23', '#7ba6bc', '#f0c667', '#ede2b3', '#672b35', '#142a36'],
+			background: '#108266'
+		}, {
+			name: 'kov_02',
+			colors: ['#e8dccc', '#e94641', '#eeaeae'],
+			background: '#6c96be'
+		}, {
+			name: 'kov_03',
+			colors: ['#e3937b', '#d93f1d', '#090d15', '#e6cca7'],
+			background: '#558947'
+		}, {
+			name: 'kov_04',
+			colors: ['#d03718', '#292b36', '#33762f', '#ead7c9', '#ce7028', '#689d8d'],
+			background: '#deb330'
+		}, {
+			name: 'kov_05',
+			colors: ['#de3f1a', '#de9232', '#007158', '#e6cdaf', '#869679'],
+			background: '#7aa5a6'
+		}, {
+			name: 'kov_06',
+			colors: [
+				'#a87c2a',
+				'#bdc9b1',
+				'#f14616',
+				'#ecbfaf',
+				'#017724',
+				'#0e2733',
+				'#2b9ae9'
+			],
+			background: '#dfd4c1'
+		}, {
+			name: 'kov_06b',
+			colors: [
+				'#d57846',
+				'#dfe0cc',
+				'#de442f',
+				'#e7d3c5',
+				'#5ec227',
+				'#302f35',
+				'#63bdb3'
+			],
+			background: '#dfd4c1'
+		}, {
+			name: 'kov_07',
+			colors: ['#c91619', '#fdecd2', '#f4a000', '#4c2653'],
+			background: '#89c2cd'
 		}
 	];
 
@@ -302,10 +357,10 @@
 			canvasPadding = 80;
 		}
 		size = window.innerWidth < window.innerHeight ? window.innerWidth - canvasPadding : window.innerHeight - canvasPadding;
-//		dpr = window.devicePixelRatio;
+		//		dpr = window.devicePixelRatio;
 		canvas.width = size;
-		canvas.height = size ;
-//		context.scale(dpr, dpr);
+		canvas.height = size;
+		//		context.scale(dpr, dpr);
 		context.lineWidth = 2;
 
 		tileStep = (size - offset * 2) / 8;
@@ -376,6 +431,7 @@
 		box-shadow: 1px 10px 15px #888888;
 		margin: 20px auto;
 	}
+
 	:global(body) {
 		margin: 0;
 		background-color: #ffffff;
